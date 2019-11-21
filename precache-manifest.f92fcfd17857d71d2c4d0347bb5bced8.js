2196bfef65d0cf2a1fb8fe8292d5561e5873fc41
= <=>
2019-11-21 15:21:17 +0700

    Kak AGA kasih aku 100, aku traktir MCD

---
+++ precache-manifest.f92fcfd17857d71d2c4d0347bb5bced8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f3aed688aca83086b6",
+    "url": "/css/about.ab692de5.css"
+  },
+  {
+    "revision": "344d0837a839090f5d45",
+    "url": "/css/chunk-vendors.583c33a2.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "30366029b171068eea9e7ff234449c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f3aed688aca83086b6",
+    "url": "/js/about.53d2665a.js"
+  },
+  {
+    "revision": "b78753a93023fa4d17da",
+    "url": "/js/app.a960280f.js"
+  },
+  {
+    "revision": "344d0837a839090f5d45",
+    "url": "/js/chunk-vendors.a0b0866c.js"
+  },
+  {
+    "revision": "b2430681130215d463f0b3ad3679bdfd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
